89fff9579e29fce38ab1a8eec094802e2947d8a6
Ana Paula Macedo <anapaulamacedo.f@gmail.com>
2022-09-30 15:50:41 +0200

    Moved my existing project inside JS folder
    workin in it...

---
+++ src/index.js
@@ -0,0 +1,93 @@
+// Feature #1
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0 ${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let weekDays = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[weekDays];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+//Homework Week 5
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  document.querySelector("#cities").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "597c40c39084687093b091cd48b366f8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function showLocation(position) {
+  let apiKey = "597c40c39084687093b091cd48b366f8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+searchCity("Berlin");
+
+//Homework Week 5
+
+// !!!Bonus Feature( About the Homework of week 4)
+//function convertFahrenheit(event) {
+// event.preventDefault();
+// let temperatureElement = document.querySelector("#temperature");
+//  temperatureElement.innerHTML = 99;}
+
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", convertFahrenheit);
+
+//function convertCelsius(event) {
+//  event.preventDefault();
+//  let temperatureElement = document.querySelector("#temperature");
+//  temperatureElement.innerHTML = 36;}
+
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", convertCelsius);
